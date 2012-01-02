06ce220b78b28f1bb99ce0f791fa99aca1a774c9
Alex Nichol <alex@Alexs-MacBook-Air.local>
2012-01-02 13:59:46 -0500

    Created typing test view file

---
+++ TypingTest/ANTypingTestView.h
@@ -0,0 +1,13 @@
+//
+//  ANTypingTestView.h
+//  TypingTest
+//
+//  Created by Alex Nichol on 1/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANTypingTestView : NSView
+
+@end
---
+++ TypingTest/ANTypingTestView.m
@@ -0,0 +1,28 @@
+//
+//  ANTypingTestView.m
+//  TypingTest
+//
+//  Created by Alex Nichol on 1/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ANTypingTestView.h"
+
+@implementation ANTypingTestView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
