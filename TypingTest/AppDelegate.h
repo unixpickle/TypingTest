df4e1ad5411ff9f671c89d0c37e0ac0589fcc7b8
Alex Nichol <alex@Alexs-MacBook-Air.local>
2012-01-02 13:39:59 -0500

    Initial Commit

---
+++ TypingTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TypingTest
+//
+//  Created by Alex Nichol on 1/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TypingTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TypingTest
+//
+//  Created by Alex Nichol on 1/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
